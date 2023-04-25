27ac1d691ef363a0f5d792bb08794ee62cb0667c
Mauricio Murua <murua5@hotmail.com>
2023-04-24 22:14:50 -0300

    section 12

--- 05-hook-app/src/hooks/useCounter.js
+++ 05-hook-app/src/hooks/useCounter.js
@@ -5,14 +5,24 @@ export const useCounter = ( initialValue = 10 ) => {
 
     const [ counter, setCounter ] = useState( initialValue )
 
+    // const increment = ( value = 1 ) => {
+    //     setCounter( counter + value );
+    // }
+
+    // const decrement = ( value = 1 ) => {
+    //     // if ( counter === 0 ) return;
+
+    //     setCounter( counter - value );
+    // }
+
     const increment = ( value = 1 ) => {
-        setCounter( counter + value );
+        setCounter( (current) => current + value );
     }
 
     const decrement = ( value = 1 ) => {
         // if ( counter === 0 ) return;
 
-        setCounter( counter - value );
+        setCounter( (current) => current - value );
     }
 
     const reset = () => {
