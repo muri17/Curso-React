ae652b03d416a173d5931aba1e0531b54317b1fd
Mauricio Murua <murua5@hotmail.com>
2023-04-27 23:03:38 -0300

    Section 15

--- 07-heroes-spa/src/ui/components/Navbar.jsx
+++ 07-heroes-spa/src/ui/components/Navbar.jsx
@@ -1,13 +1,17 @@
+import { useContext } from 'react';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../auth';
 
 
 export const Navbar = () => {
 
+    const { user, logout } = useContext( AuthContext )
     const navigate = useNavigate()
     
     const onLogout = () => {
-        navigate('/', {
-        replace: true
+        logout();
+        navigate('/login', {
+            replace: true
         });
     }
 
@@ -50,7 +54,7 @@ export const Navbar = () => {
             <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
                 <ul className="navbar-nav ml-auto">
                     <span className="nav-item nav-link text-primary">
-                        Mauricio
+                        { user?.name }
                     </span>
 
                     <button className="nav-item nav-link btn" onClick={onLogout}>
